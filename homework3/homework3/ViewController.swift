a0f2eb6d1a6cb54a061fb073e7a3992fe41fd2f3
tataka-e <841254134@qq.com>
2021-03-20 20:06:59 -0700

    work3

---
+++ homework3/homework3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework3
+//
+//  Created by 0commander on 2021/3/20.
+//  Copyright © 2021 0commander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
